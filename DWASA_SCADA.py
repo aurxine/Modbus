97a3e65b5e6a31f7c86169502b62996ff7c54c40
aurxine <sarowardhrubo@gmail.com>
2021-01-07 14:56:03 +0600

    back to prev freq settings

--- DWASA_SCADA.py
+++ DWASA_SCADA.py
@@ -243,10 +243,10 @@ class SCADA_Devices():
         elif command["Command"] == "Change_VFD_Frequency":
             frequency = int(command["Frequency"])*100
             response = self.VFD.writeRunningFrequency(frequency_value= frequency)
-            if response == 1:
-                self.publish(topic= self.mqtt_pub_topic, payload= "Frequency change " + str(frequency/100) + " Hz was successfull")
-            else:
+            if response == 0:
                 self.publish(topic= self.mqtt_pub_topic, payload= "Frequency change " + str(frequency/100) + " Hz was unsuccessfull")
+            else:
+                self.publish(topic= self.mqtt_pub_topic, payload= "Frequency change " + str(frequency/100) + " Hz was successfull")
 
         elif command["Command"] == "Change_Past_Water_Flow":
             past_water_flow = int(command["Past_Water_Flow"])
--- VFD.py
+++ VFD.py
@@ -207,12 +207,12 @@ class VFD_F800():
             # Writing to a holding register with the below content.
             self.client.write_register(address=1000, value = frequency_value)
             
-            for i in range(50):
-                time.sleep(0.1)
-                frequency = self.readOutputFrequency()
-                if frequency == frequency_value:
-                    return 1
-            return 0
+            # for i in range(50):
+            #     time.sleep(0.1)
+            #     frequency = self.readOutputFrequency()
+            #     if frequency == frequency_value:
+            #         return 1
+            # return 0
             
         else:
             print('Cannot connect to the VFD')
