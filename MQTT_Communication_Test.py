c8273e43c60e27c8cd79e4ac2d5eab9f4ce26d0b
aurxine <sarowardhrubo@gmail.com>
2021-01-11 14:41:12 +0600

    use loop_start() instead of loop()

--- MQTT_Communication_Test.py
+++ MQTT_Communication_Test.py
@@ -13,14 +13,25 @@ init = pd.read_csv(init_file)
 params = list(init['parameters'])
 
 #Get Broker, Port, Subscription Topic and Publish Topic
-pub_topic = init.iloc[params.index('sub_topic'), 1]
-sub_topic = init.iloc[params.index('pub_topic'), 1]
+pub_topic = "Test"#init.iloc[params.index('sub_topic'), 1]
+sub_topic = "Test"#init.iloc[params.index('pub_topic'), 1]
 broker_address = init.iloc[params.index('broker_address'), 1]
 broker_port = int(init.iloc[params.index('broker_port'), 1])
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected")
+    if rc==0:
+        client.connected_flag=True #set flag
+        print("connected OK")
+    else:
+        print("Bad connection Returned code=",rc)
+        client.bad_connection_flag=True
+
+def on_disconnect(client, userdata, rc):
+    # logging.info("disconnecting reason  "  +str(rc))
+    print("Client disconnected")
+    client.connected_flag=False
+    client.disconnect_flag=True
 
 def on_message(client, userdata, message):
     print("Topic: " + message.topic)
@@ -29,12 +40,27 @@ def on_message(client, userdata, message):
 
 MQTT_Client = mqtt.Client("Client", transport='websockets')
 MQTT_Client._on_connect = on_connect
+MQTT_Client.on_disconnect = on_disconnect
 MQTT_Client.on_message = on_message
 
-MQTT_Client.connect("123.49.33.109", port = 8083)
-MQTT_Client.subscribe(sub_topic)
+try:
+    print("Trying to connect to broker")
+    MQTT_Client.connect(broker_address, broker_port) #connect to broker
+except:
+    print("connection failed")
+    exit(1) #Should quit or raise flag to quit or retry
+
+try:
+    print("Subscribing to topic")
+    MQTT_Client.subscribe(sub_topic)
+except:
+    print("Subscription failed")
 
 
+count = 0
+MQTT_Client.loop_start()
 while True:
-    MQTT_Client.publish(topic=sub_topic, payload="Hello")
-    time.sleep(1)
+    # MQTT_Client.loop()
+    count += 1
+    MQTT_Client.publish(topic=pub_topic, payload="message count: " + str(count))
+    time.sleep(2)
